4998b71dcb539e0eb729379258444b7dd522bb4b
Üftade Bengi Erolçay <56762554+uftadeerolcay@users.noreply.github.com>
2023-09-26 15:56:04 +0300

    dvdrental_2
    On dvdrental dataset "IN" operator is used

---
+++ odev2.sql
@@ -0,0 +1,16 @@
+--film tablosunda bulunan tüm sütunlardaki verileri replacement cost değeri
+--12.99 dan büyük eşit ve 16.99 küçük olma koşuluyla sıralayınız ( BETWEEN - AND yapısını kullanınız.)
+select * from film
+where replacement_cost BETWEEN 12.99 AND 16.99
+ORDER BY replacement_cost DESC;
+
+--.actor tablosunda bulunan first_name ve last_name sütunlardaki verileri first_name
+--'Penelope' veya 'Nick' veya 'Ed' değerleri olması koşuluyla sıralayınız. ( IN operatörünü kullanınız.)
+select first_name,last_name from actor
+where first_name IN ('Penelope', 'Nick', 'Ed');
+
+--film tablosunda bulunan tüm sütunlardaki verileri
+--rental_rate 0.99, 2.99, 4.99 VE replacement_cost 12.99, 15.99, 28.99
+--olma koşullarıyla sıralayınız. ( IN operatörünü kullanınız.)
+select * from film
+where rental_rate IN ('0.99', '2.99', '4.99') AND replacement_cost IN ('12.99', '15.99', '28.99');
